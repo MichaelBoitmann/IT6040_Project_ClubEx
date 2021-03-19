c40b0fda2be21b4b229994210b0c76dd2902acc1
joelHilley <66706087+joelHilley@users.noreply.github.com>
2021-03-19 16:22:04 +1300

    HttpRequest test

--- clubexproject/clubexapp/views.py
+++ clubexproject/clubexapp/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse, render
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. Yay it works.")
