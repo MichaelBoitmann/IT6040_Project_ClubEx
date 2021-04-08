c645653c5fbf4e44d3978a7b3345a1027845c04f
joelHilley <66706087+joelHilley@users.noreply.github.com>
2021-04-08 14:47:57 +1200

    videos posted on home page
    vertical categories with horizontal video(s)

--- clubexproject/clubexapp/admin.py
+++ clubexproject/clubexapp/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Program
 
-admin.site.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+        list_display = ('title','category',)
+        search_fields = ('title','category',)
+
+        filter_horizontal = ()
+        list_filter = ()
+        fieldsets = ()
+
+admin.site.register(Program, ProgramAdmin)
