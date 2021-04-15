6dbd03c61e6148be7cae80dc1c9b019116b26391
joelHilley <66706087+joelHilley@users.noreply.github.com>
2021-04-15 14:57:16 +1200

    update model

--- clubexproject/clubexapp/models.py
+++ clubexproject/clubexapp/models.py
@@ -2,18 +2,9 @@ from django.db import models
 
 from embed_video.fields import EmbedVideoField
 
-#***un-used model for now ***#
-
-class Program(models.Model):
-   title = models.CharField(max_length=30)
-   category = models.CharField(max_length=30)
-   video_url = models.CharField(max_length=200)
-    # number_of_views...
-    # rating...
-
 class Video(models.Model):
     title = models.CharField(max_length=100)
-    #category = models.CharField(max_length=30)
+    category = models.CharField(max_length=30, default="No Category Selected")
     added = models.DateTimeField(auto_now_add=True)
     url = EmbedVideoField()
 
