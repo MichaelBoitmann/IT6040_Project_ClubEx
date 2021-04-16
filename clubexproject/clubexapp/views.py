c5cc5bd4b60bb9b4c3b36fdad53ff37624fe9104
joelHilley <66706087+joelHilley@users.noreply.github.com>
2021-04-16 14:27:47 +1200

    category model added
    as foreign key for videos model

--- clubexproject/clubexapp/views.py
+++ clubexproject/clubexapp/views.py
@@ -19,3 +19,16 @@ class CustomerPageView(TemplateView):
 def video_index(request):
     videos = Video.objects.all()
     return render(request, 'videos_folder/video_tube.html', context={'videos': videos})
+
+#@login_required
+# #def category(request, pk):
+#     ca_exercise = Exercise.objects.filter(category=pk).order_by('exercise_name')
+#     categories = Category.objects.all().order_by('category_name')
+#     return render(request, 'categories.html', {'pk':pk,'ca_exercise':ca_exercise, 'categories':categories})
+#
+# #@login_required
+# def video(request):
+#     category_exercise = video.objects.all().order_by('-views')
+#     featured = category_exercise[0]
+#     categories = Category.objects.all().order_by('category_name')
+#     return render(request, 'exercise.html', {'category_exercise':category_exercise, 'categories':categories, 'featured':featured})
